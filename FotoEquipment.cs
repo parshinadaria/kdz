211eeaa8c79bf9a84d37e5c090d0c01dfc3c1f47
parshinadaria <parshinadaria14@mail.ru>
2017-03-14 09:44:02 +0300

    Add files via upload
    classes

---
+++ FotoEquipment.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    abstract class FotoEquipment
+    {
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+        private double weight;
+
+        public double Weight
+        {
+            get { return weight; }
+        }
+
+        private string country;
+
+        public string Country
+        {
+            get { return country; }
+        }
+
+        private string company;
+
+        public string Company
+        {
+            get { return company; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+        }
+
+        public FotoEquipment(double Price, double Weight, string Country, string Company, string Model)
+        {
+            price = Price;
+            weight = Weight;
+            country = Country;
+            company = Company;
+            model = Model;
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClassLibrary
+{
+    class Repository
+    {
+        List<FotoEquipment> ListFotoEquipment;
+        public void ReadFromFile(string filename)
+        {
+            string Line = "";
+            StreamReader sr = new StreamReader(filename);
+            while ((Line=sr.ReadLine())!= null)
+            {
+                string[] fileInfo = Line.Split(new char[] { ' ' });
+                FotoEquipment f;
+                if (fileInfo[0] == "Camera")
+                {
+                    f = new Camera(double.Parse(fileInfo[1]), double.Parse(fileInfo[2]))
+                }
+                else
+                    ListFotoEquipment.Add(f);    
+            }
+            sr.Close();
+
+        }
+        public Repository()
+        {
+            ListFotoEquipment = new List<FotoEquipment>();
+        }
+
+    }
+}
